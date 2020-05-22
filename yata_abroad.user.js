92ccfc2a9755b6b318f5a46e27a03bb513b084ad
Pyrit[2111649] <44345987+TotallyNot@users.noreply.github.com>
2020-05-23 00:34:29 +0200

    initial commit

---
+++ yata_abroad.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         YATA abroad
+// @namespace    yata.alwaysdata.net
+// @version      0.1
+// @updateURL    https://github.com/TotallyNot/yata-abroad/raw/master/js/yata_abroad.user.js
+// @description  Update item stocks abroad for YATA
+// @author       Pyrit[2111649]
+// @match        https://www.torn.com/index.php
+// @grant        GM.xmlHttpRequest
+// @connect      yata.alwaysdata.net
+// @run-at       document-end
+// ==/UserScript==
+
+function scrapeStock() {
+    const items = document.querySelectorAll(".item-info-wrap");
+
+    return [...items].map((item) => ({
+        id: parseInt(item.querySelector("[id^=item]").id.match(/[0-9]+/)[0]),
+        stock: parseInt(item.querySelector(".stck-amount").innerText),
+        cost: parseInt(
+            item.querySelector(".c-price").innerText.replace(/[^0-9]+/g, "")
+        ),
+    }));
+}
+
+function update(items) {
+    GM.xmlHttpRequest({
+        url: "https://yata.alwaysdata.net/bazaar/abroad/import/",
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        data: JSON.stringify({
+            country: document
+                .querySelector(".content-title > h4")
+                .innerText.substr(0, 3)
+                .toLowerCase(),
+            client: "YATA abroad userscript",
+            uid: parseInt(
+                document
+                    .querySelector('script[src^="/js/chat/chat"]')
+                    .getAttribute("uid")
+            ),
+            items,
+        }),
+        onload: (response) => console.log(response),
+    });
+}
+
+const items = scrapeStock();
+if (items) {
+    update(items);
+}
